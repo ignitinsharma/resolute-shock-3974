02803f910e9b6703e0f9f62b0c29132f17907916
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-11-10 21:07:45 +0530

    added

---
+++ beautybebo/src/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Box, Flex, Heading, HStack, Spacer, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <Flex
+      minWidth="full"
+      minHeight="80px"
+      alignItems="center"
+      gap="2"
+      py={6}
+      px={4}
+      bg="white"
+    >
+      <Link to="/">
+        <Box>
+         
+        </Box>
+      </Link>
+      <Spacer />
+      <HStack>
+        <Link to="/login">
+          <Text fontSize="lg" fontWeight="bolder">LOG IN</Text>
+        </Link>
+        <Link to="/signup" >
+          <Text fontSize="lg" fontWeight="bolder" >SIGN UP</Text>
+        </Link>
+      </HStack>
+    </Flex>
+  );
+}
+
+export default Navbar;
