0ec3179371c034f04600c622a22433e02b5b09d3
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-11-10 21:05:23 +0530

    added

---
+++ beautybebo/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Allroutes from './Components/Allroutes';
+import Navbar1 from './Components/Navbar1';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Navbar1/>
+      <Allroutes/>
+    </div>
+  );
+}
+
+export default App;
